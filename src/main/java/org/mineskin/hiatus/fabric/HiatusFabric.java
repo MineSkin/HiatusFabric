6ac584ced2fcfa2451570ebff1f4e9c2bfda0f60
inventivetalent <mail@inventivetalent.org>
2022-10-03 15:15:14 +0200

    1.19.2 support

--- src/main/java/org/mineskin/hiatus/fabric/HiatusFabric.java
+++ src/main/java/org/mineskin/hiatus/fabric/HiatusFabric.java
@@ -2,20 +2,16 @@ package org.mineskin.hiatus.fabric;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.Version;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.ClickEvent;
-import net.minecraft.text.HoverEvent;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,8 +23,8 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.argument;
-import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class HiatusFabric implements ModInitializer {
 
@@ -72,109 +68,110 @@ public class HiatusFabric implements ModInitializer {
             hiatus.onGameClosing();
         }));
 
-        Command<FabricClientCommandSource> help = context -> {
-            context.getSource().sendFeedback(new LiteralText("\n")
-                    .append(new LiteralText("/mineskin hiatus list\n")
+        Command<ServerCommandSource> help = context -> {
+            context.getSource().sendMessage(Text.literal("\n")
+                    .append(Text.literal("/mineskin hiatus list\n")
                             .styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/mineskin hiatus list"))))
-                    .append(new LiteralText("/mineskin hiatus add <token> <email>\n")
+                    .append(Text.literal("/mineskin hiatus add <token> <email>\n")
                             .styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/mineskin hiatus add "))))
-                    .append(new LiteralText("/mineskin hiatus remove <email>\n")
+                    .append(Text.literal("/mineskin hiatus remove <email>\n")
                             .styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/mineskin hiatus remove "))))
                     .append(Text.of("\n"))
-                    .append(new LiteralText("[Login on mineskin.org]")
+                    .append(Text.literal("[Login on mineskin.org]")
                             .styled(style -> style.withBold(true)
                                     .withColor(Formatting.YELLOW)
                                     .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://mineskin.org/account?hiatus=true"))
-                                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText("Click to go to mineskin.org")))
+                                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to go to mineskin.org")))
                             ))
                     .append(Text.of(" to link your account"))
             );
             return Command.SINGLE_SUCCESS;
         };
 
-        CommandDispatcher<FabricClientCommandSource> dispatcher = ClientCommandManager.DISPATCHER;
-        dispatcher.register(literal("mineskin")
-                .executes(help));
-        dispatcher.register(literal("mineskin")
-                .then(literal("hiatus")
-                        .executes(help)));
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(literal("mineskin")
+                    .executes(help));
+            dispatcher.register(literal("mineskin")
+                    .then(literal("hiatus")
+                            .executes(help)));
 
-        dispatcher.register(literal("mineskin")
-                .then(literal("hiatus")
-                        .then(literal("list")
-                                .executes(context -> {
-                                    hiatus.getAccounts().thenAccept(accounts -> {
-                                        if (accounts.isEmpty()) {
-                                            context.getSource().sendFeedback(Text.of("No accounts registered"));
-                                        } else {
-                                            LiteralText text = new LiteralText("\nRegistered accounts:\n");
-                                            for (Map.Entry<UUID, String> account : accounts.entrySet()) {
-                                                text.append(new LiteralText(account.getValue() + " " + account.getKey()))
-                                                        .append(new LiteralText(" [Remove]\n")
-                                                                .styled(style -> style.withBold(true)
-                                                                        .withColor(Formatting.RED)
-                                                                        .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mineskin hiatus remove " + account.getValue()))
-                                                                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText("Click to remove")))
-                                                                )
-                                                        );
+            dispatcher.register(literal("mineskin")
+                    .then(literal("hiatus")
+                            .then(literal("list")
+                                    .executes(context -> {
+                                        hiatus.getAccounts().thenAccept(accounts -> {
+                                            if (accounts.isEmpty()) {
+                                                context.getSource().sendMessage(Text.literal("No accounts registered"));
+                                            } else {
+                                                MutableText text = Text.literal("\nRegistered accounts:\n");
+                                                for (Map.Entry<UUID, String> account : accounts.entrySet()) {
+                                                    text.append(Text.literal(account.getValue() + " " + account.getKey())
+                                                            .append(Text.literal(" [Remove]\n")
+                                                                    .styled(style -> style.withBold(true)
+                                                                            .withColor(Formatting.RED)
+                                                                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/mineskin hiatus remove " + account.getValue()))
+                                                                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, MutableText.of(new LiteralTextContent("Click to remove"))))
+                                                                    )
+                                                            ));
+                                                }
+                                                context.getSource().sendMessage(text);
                                             }
-                                            context.getSource().sendFeedback(text);
-                                        }
-                                    });
-                                    return Command.SINGLE_SUCCESS;
-                                }))
-                ));
-        dispatcher.register(literal("mineskin")
-                .then(literal("hiatus")
-                        .then(literal("add")
-                                .then(argument("token", StringArgumentType.word())
-                                        .then(argument("email", StringArgumentType.greedyString())
-                                                .executes(context -> {
-                                                    String token = context.getArgument("token", String.class);
-                                                    String email = context.getArgument("email", String.class);
+                                        });
+                                        return Command.SINGLE_SUCCESS;
+                                    }))
+                    ));
+            dispatcher.register(literal("mineskin")
+                    .then(literal("hiatus")
+                            .then(literal("add")
+                                    .then(argument("token", StringArgumentType.word())
+                                            .then(argument("email", StringArgumentType.greedyString())
+                                                    .executes(context -> {
+                                                        String token = context.getArgument("token", String.class);
+                                                        String email = context.getArgument("email", String.class);
 
-                                                    GameProfile profile = MinecraftClient.getInstance().getSession().getProfile();
-                                                    if (profile != null) {
-                                                        if (!MineSkinHiatus.validateTokenAccount(profile.getId(), token)) {
-                                                            context.getSource().sendError(Text.of("Please launch the game with the account you're trying to link (invalid token)"));
-                                                            return Command.SINGLE_SUCCESS;
+                                                        GameProfile profile = MinecraftClient.getInstance().getSession().getProfile();
+                                                        if (profile != null) {
+                                                            if (!MineSkinHiatus.validateTokenAccount(profile.getId(), token)) {
+                                                                context.getSource().sendError(Text.literal("Please launch the game with the account you're trying to link (invalid token)"));
+                                                                return Command.SINGLE_SUCCESS;
+                                                            }
+
+                                                            hiatus.setAccount(new Account(profile.getId(), email, token));
+                                                            context.getSource().sendMessage(Text.literal("\nAccount added!"));
+                                                            hiatus.onGameLaunching(); // send as if the game just launched
+                                                        } else {
+                                                            context.getSource().sendError(Text.literal("Failed to add account, no game profile found!"));
                                                         }
 
-                                                        hiatus.setAccount(new Account(profile.getId(), email, token));
-                                                        context.getSource().sendFeedback(Text.of("\nAccount added!"));
-                                                        hiatus.onGameLaunching(); // send as if the game just launched
+                                                        return Command.SINGLE_SUCCESS;
+                                                    }))))));
+            dispatcher.register(literal("mineskin")
+                    .then(literal("hiatus")
+                            .then(literal("remove")
+                                    .then(argument("email", StringArgumentType.greedyString())
+                                            .executes(context -> {
+                                                String email = context.getArgument("email", String.class);
+
+                                                hiatus.getAccounts().thenAccept(accounts -> {
+                                                    if (accounts.isEmpty()) {
+                                                        context.getSource().sendError(Text.literal("No accounts registered"));
                                                     } else {
-                                                        context.getSource().sendError(Text.of("Failed to add account, no game profile found!"));
+                                                        Map<String, UUID> invertedAccounts = accounts.entrySet()
+                                                                .stream()
+                                                                .collect(Collectors.toMap(e -> e.getValue().toLowerCase(), Map.Entry::getKey));
+                                                        UUID uuid = invertedAccounts.get(email.toLowerCase());
+                                                        if (uuid == null) {
+                                                            context.getSource().sendError(Text.literal("Account not found"));
+                                                            return;
+                                                        }
+                                                        hiatus.deleteAccount(uuid);
+                                                        context.getSource().sendMessage(Text.literal("\nAccount removed!"));
                                                     }
+                                                });
 
-                                                    return Command.SINGLE_SUCCESS;
-                                                }))))));
-        dispatcher.register(literal("mineskin")
-                .then(literal("hiatus")
-                        .then(literal("remove")
-                                .then(argument("email", StringArgumentType.greedyString())
-                                        .executes(context -> {
-                                            String email = context.getArgument("email", String.class);
-
-                                            hiatus.getAccounts().thenAccept(accounts -> {
-                                                if (accounts.isEmpty()) {
-                                                    context.getSource().sendError(Text.of("No accounts registered"));
-                                                } else {
-                                                    Map<String, UUID> invertedAccounts = accounts.entrySet()
-                                                            .stream()
-                                                            .collect(Collectors.toMap(e -> e.getValue().toLowerCase(), Map.Entry::getKey));
-                                                    UUID uuid = invertedAccounts.get(email.toLowerCase());
-                                                    if (uuid == null) {
-                                                        context.getSource().sendError(Text.of("Account not found"));
-                                                        return;
-                                                    }
-                                                    hiatus.deleteAccount(uuid);
-                                                    context.getSource().sendFeedback(Text.of("\nAccount removed!"));
-                                                }
-                                            });
-
-                                            return Command.SINGLE_SUCCESS;
-                                        })))));
+                                                return Command.SINGLE_SUCCESS;
+                                            })))));
+        });
 
     }
 
